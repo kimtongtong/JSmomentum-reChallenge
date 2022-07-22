9ec8785297e0e1488fc192c9765a347b907adf29
kimtongtong <93583978+kimtongtong@users.noreply.github.com>
2022-07-23 06:27:10 +0900

    weather

--- src/weather.js
+++ src/weather.js
@@ -4,16 +4,22 @@ const API_KEY = "1612ca150a59a697baa79bf2b766a295";
 function success(position){ //position은 인자값은 디버깅 했을때 "허용"을 발생하는 이벤트 정보 값이다.//
   const lat = position.coords.latitude; //geolocation 메소드에서 발생한 이벤트 정보 json 값의 일부//
   const lng = position.coords.longitude;
+  console.log(position) //position은 navigator 함수를 실행 할때 현재 위치에 대한 이벤트 정보이다.//
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`; //openweathermap 사이트에서 가져온 api(cuurent weather data)의 url 주소. 주소 안에  lat, lng, 그리고 api 키를 넣으면 된다//
   fetch(url) //fetch 함수를 사용하여 url을 가져오며 "검사"를 해보면 network에 weather이 생기는 것을 알 수 있다.//
     .then(response => response.json()) //url을 가져온 다음 then을 사용하여 response 값을 가져와야 한다. json은 geloca의 오브젝트 파일의 정보 모두를 뜻한다.//
-    .then(data => { //화면에 나오게 하기 위해 const로 선언을 해준 후 html에 넣어주도록 한다.//
+    //response은 network에서 weather 정보를 누르면 respose 목록이 나온다. 거기서 받아 오는 것 같다.//
+    .then(info => { //info는 함수의 인자 같다. 이름은 상관이 없는 듯 하다.//
       const placeName = document.querySelector("#weather span:first-child");
-      const placeWeather = document.querySelector("#weather span:last-child");
-    const name = data.name;
-    const weather = data.weather[0].main;
+      const placeWeather = document.querySelector("#weather span:nth-child(2)");
+      const weatherTemp = document.querySelector("#weather span:last-child");
+    const name = info.name;
+    const weather = info.weather[0].main; //weather뒤에 [0] 을 쓰는 이유는 [배열]안 {오브젝트}로 되어있기떄문에 배열[오브젝트]를 가져와야 한다.
+    const temp = info.main.temp;
+    console.log(info) //openweather api에서 가져온 주소에 대한 이벤트 정보이다.//
     placeName.innerText = name;
     placeWeather.innerText = weather;
+    weatherTemp.innerText = temp;
   }); //허용 했을떄의 실행함수//
 }
 function error() {
